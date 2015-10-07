7b220d9074bc196840e5794a2d2e69df9e636dc6
eduin ivan hernandez garnica <eduin_ivan@hotmail.com>
2015-10-07 16:32:43 -0500

    Agregando materializecss  y modificando las dependencias

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/firebase/firebase.js',
       'bower_components/angularfire/dist/angularfire.js',
@@ -32,6 +33,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/Materialize/bin/materialize.js',
       'bower_components/mockfirebase/browser/mockfirebase.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
